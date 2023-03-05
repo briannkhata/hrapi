702de83adfd492a2092d0569ab5f347a04c4306f
briannkhata <briannkhata@gmail.com>
2023-03-05 11:31:28 +0200

    initial commit

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace hrapi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hrapi.WebApiApplication" Language="C#" %>
